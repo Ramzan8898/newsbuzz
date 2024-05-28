c077c8d94ed7a33c7de972bb4a9664c3451404ee
Ramzan8898 <muhammadramzan3043@gmail.com>
2024-05-28 16:36:18 +0500

    working on bootstrap import

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <h1>This is Navbar Component</h1>
+        )
+    }
+}
+
+export default Navbar;
