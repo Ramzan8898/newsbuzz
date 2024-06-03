42b5115c5b55b85c99dfe1cab0c91b16304dce09
Ramzan8898 <muhammadramzan3043@gmail.com>
2024-06-03 14:48:47 +0500

    add logic to control render empty pages using previous and Next

--- src/components/News.js
+++ src/components/News.js
@@ -4,27 +4,58 @@ export class News extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            articles: []
+            articles: [],
+            page: 1
         };
     }
+
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5db21f6c18124fb7857f6cd9f8f3119f";
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5db21f6c18124fb7857f6cd9f8f3119f&pageSize=16";
         let data = await fetch(url);
         let news = await data.json();
         console.log(news);
         this.setState({
-            articles: news.articles
+            articles: news.articles,
+            totalResults: news.totalResults
         })
     }
+
+    handleNext = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 16)) {
+
+        } else {
+            let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5db21f6c18124fb7857f6cd9f8f3119f&page=${this.state.page + 1}&pageSize=16`;
+            let data = await fetch(url);
+            let news = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articles: news.articles
+            });
+        }
+    }
+    handlePrev = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5db21f6c18124fb7857f6cd9f8f3119f&page=${this.state.page - 1}`;
+        let data = await fetch(url);
+        let news = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: news.articles
+        });
+    }
+
     render() {
         return (
-            <div>
-                <div className="row container mx-5">
+            <div className='my-5'>
+                <div className="row container mx-5 ">
                     {this.state.articles.map((element, index) => {
                         return <div className='col-md-3' key={index}>
-                            <NewsBox title={element.title} desc={element.description} imageUrl={element.urlToImage} postUrl={element.url}/>
+                            <NewsBox title={element.title} desc={element.description} imageUrl={element.urlToImage} postUrl={element.url} />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" onClick={this.handlePrev} className="btn btn-dark btn-sm">&larr; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 16)} type="button" onClick={this.handleNext} className="btn btn-dark btn-sm">Next &rarr;</button>
                 </div>
             </div>
         )
