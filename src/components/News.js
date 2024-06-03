89e74f0a271570e9efe484ce74fff83c163eab30
Ramzan8898 <muhammadramzan3043@gmail.com>
2024-06-03 11:43:29 +0500

    complete the data on news car

--- src/components/News.js
+++ src/components/News.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 import NewsBox from './NewsBox'
 export class News extends Component {
     constructor(props) {
-        super();
+        super(props);
         this.state = {
-            articles: this.articles
+            articles: []
         };
     }
     async componentDidMount() {
         let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5db21f6c18124fb7857f6cd9f8f3119f";
         let data = await fetch(url);
         let news = await data.json();
+        console.log(news);
         this.setState({
             articles: news.articles
         })
@@ -18,10 +19,10 @@ export class News extends Component {
     render() {
         return (
             <div>
-                <div className="container d-flex justify-content-between align-items-center">
-                    {this.state.articles.map((element) => {
-                        return <div className='col-md-4'>
-                            <NewsBox title="{element.title}" desc="{element.description}" />
+                <div className="row container mx-5">
+                    {this.state.articles.map((element, index) => {
+                        return <div className='col-md-3' key={index}>
+                            <NewsBox title={element.title} desc={element.description} imageUrl={element.urlToImage} postUrl={element.url}/>
                         </div>
                     })}
                 </div>
